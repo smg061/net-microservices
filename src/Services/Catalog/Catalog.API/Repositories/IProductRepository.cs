8711b0b7e9e877c1791b08380d78ccefda2cb5ad
Santos Gonzalez <max.go.95@gmail.com>
2022-05-26 23:45:05 -0500

    initial entity and repository layer setup

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,24 @@
+using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+
+    public interface IProductRepository {
+
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductsByCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+
+
+
+    }
+}
